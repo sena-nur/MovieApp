ff23d0751438289af98bbda4f08f574257323c66
senanur.kose <senanur.kose@piworks.net>
2024-08-12 18:15:34 +0300

    feat: add movie model

---
+++ src/app/movie.ts
@@ -0,0 +1,4 @@
+export class Movie {
+  id: number | undefined;
+  name: String | undefined;
+}
--- src/app/movies/movies.component.ts
+++ src/app/movies/movies.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { MovieComponent } from '../movie/movie.component';
+import { Movie } from '../movie';
 
 @Component({
   imports: [MovieComponent],
@@ -8,4 +9,15 @@ import { MovieComponent } from '../movie/movie.component';
   templateUrl: 'movies.component.html',
   styleUrl: 'movies.component.css',
 })
-export class MoviesComponent {}
+export class MoviesComponent {
+  tite = 'Movie List';
+
+  movie: Movie = {
+    id: 1,
+    name: 'Movie Name',
+  };
+
+  getTitle() {
+    return this.tite;
+  }
+}
