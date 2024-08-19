1ba6bf8664cc0d9b781f44ed2b1ce34627b3acfd
senanur.kose <senanur.kose@piworks.net>
2024-08-19 19:04:48 +0300

    feat:add routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,18 @@ import { Component } from '@angular/core';
 import { MoviesComponent } from './movies/movies.component';
 import { CommonModule } from '@angular/common';
 import { LoggingComponent } from './logging/logging.component';
+import { RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, MoviesComponent, LoggingComponent],
+  imports: [
+    CommonModule,
+    MoviesComponent,
+    LoggingComponent,
+    RouterLink,
+    RouterOutlet,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { MoviesComponent } from './movies/movies.component';
+import { MovieDetailComponent } from './movie-detail/movie-detail.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'movies',
+    component: MoviesComponent,
+    title: 'Movie page',
+  },
+  /*   {
+        path: 'details/:id',
+        component: MovieDetailComponent,
+        title: 'Movie details',
+      }, */
+];
